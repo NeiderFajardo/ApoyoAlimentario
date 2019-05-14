feaec42aeaedd0372f9674198b898d1b11ed0a3f
NeiderFajardo <nafajardo01@gmail.com>
2019-05-13 21:45:29 -0500

    Dependiendo delrol del usuario que ingresa a la aplicación se habilitan las opciones que corresponden a cada uno, por ahora el rol esta en el código, mientras se logra hacer la conexión con la base de datos. Se agrega el modulo de realizar una solicitud solo para los estudiantes, el administrador puede dirigirse a la página que le permite agregar un nuevo usuario, se puede cerrar la sesión y dirigirse al index

--- src/servlets/inicio/LoginServlet.java
+++ src/servlets/inicio/LoginServlet.java
@@ -20,13 +20,11 @@ public class LoginServlet extends HttpServlet {
         contrasena = req.getParameter("contrasena").trim();
         resp.setContentType("text/html");
         if(verificarCuenta(usuario, contrasena)){
-
+            resp.setContentType("text/html");
             PrintWriter out = resp.getWriter();
-            out.write("<html>" +
-                    "<body>" +
-                    "<h1>El usuario: "+usuario+" ingreso con éxito</h1>" +
-                    "</body>" +
-                    "</html>");
+            //Se debe especificar el rol del usuario que ingreso al sistema, esto se hará depende del rol en la conexión de la base
+            //de datos
+            out.write(armarPaginaPrincipal("admin"));
             out.close();
         }else{
             resp.setContentType("text/html");
@@ -63,5 +61,84 @@ public class LoginServlet extends HttpServlet {
 
         return exito;
 
+    }
+    //Este método armara la vista Principal del aplicativo, en caso de que sea un estudiante se le habilitaran algunas opciones
+    //y se bloquearan otras, de igual forma si es administrador o revisor
+    //rol: Rol del usuario en la base de datos, roles posibles: {estudiante, admin, revisor}
+    private String armarPaginaPrincipal(String rol){
+        String pagina = "";
+        pagina += "<!DOCTYPE html>\n" +
+                "<html lang=\"en  \" dir=\"ltr\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"utf-8\">\n" +
+                "    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">\n" +
+                "    <link rel=\"stylesheet\", type=\"text/css\", href=\"estilos/styles.css\"/>\n" +
+                "    <link rel=\"stylesheet\", type=\"text/css\", href=\"estilos/principal.css\"/>\n" +
+                "    <link href=\"https://fonts.googleapis.com/css?family=Heebo\" rel=\"stylesheet\">\n" +
+                "    <title>Apoyo Alimentario</title>\n" +
+                "</head>\n" +
+                "<nav class=\"navbar navbar-expand-lg navbar-dark bg-primary\">\n" +
+                "\n" +
+                "    <a class=\"navbar-brand\" href=\"#\">\n" +
+                "        <img src=\"imgs/logoud_0.png\" width=\"30\" height=\"30\" class=\"d-inline-block align-top\" alt=\"\">\n" +
+                "        Apoyo UD\n" +
+                "    </a>\n" +
+                "    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n" +
+                "        <ul class=\"navbar-nav mr-auto\">\n" +
+                "            <li class=\"nav-item\">\n";
+                if (rol.equalsIgnoreCase("estudiante")) {
+                    pagina +=  "                <a class=\"nav-link\" href=\"#\">Mi solicitud</a>\n";
+                }else if(rol.equalsIgnoreCase("admin")) {
+                    pagina +=  "                <a class=\"nav-link disabled\" href=\"#\">Mi solicitud</a>\n";
+                }else if(rol.equalsIgnoreCase("revisor")){
+                    pagina +=  "                <a class=\"nav-link disabled\" href=\"#\">Mi solicitud</a>\n";
+                }
+                pagina +=      "            </li>\n" +
+                "            <li class=\"nav-item dropdown\">\n" +
+                "                <a class=\"nav-link\" href=\"#\">Mis Datos</a>\n" +
+                "            </li>\n" +
+                "            <li class=\"nav-item\">\n";
+                if(rol.equalsIgnoreCase("estudiante")) {
+                 pagina+=   "                <a class=\"nav-link disabled\" href=\"#\">Revisar Solicitudes</a>\n";
+                }else if(rol.equalsIgnoreCase("admin")){
+                    pagina+=   "                <a class=\"nav-link disabled\" href=\"#\">Revisar Solicitudes</a>\n";
+                }else if(rol.equalsIgnoreCase("revisor")){
+                    pagina+=   "                <a class=\"nav-link\" href=\"#\">Revisar Solicitudes</a>\n";
+                }
+                pagina +="            </li>\n" +
+                "            <li class=\"nav-item\">\n";
+                if (rol.equalsIgnoreCase("estudiante")) {
+                    pagina += "                <a class=\"nav-link disabled\" href=\"#\">Registrar Ususario</a>\n";
+                }else if(rol.equalsIgnoreCase("admin")){
+                    pagina += "                <a class=\"nav-link\" href=\"registro.html\">Registrar Ususario</a>\n";
+                }else if(rol.equalsIgnoreCase("revisor")){
+                    pagina += "                <a class=\"nav-link disabled\" href=\"#\">Registrar Ususario</a>\n";
+                }
+                pagina += "            </li>\n" +
+                "            <li class=\"nav-item\">\n" +
+                "                <a class=\"nav-link\" href=\"index.html\">Cerrar Sesion</a>\n" +
+                "            </li>\n" +
+                "        </ul>\n" +
+                "    </div>\n" +
+                "</nav>\n" +
+                "<header class=\"titulo\">\n" +
+                "    <h1>Programa Apoyo Alimentario</h1>\n" +
+                "    <img src=\"imgs/logoud_0.png\" alt=\"logoUd\" width=\"100\" height=\"100\">\n" +
+                "</header>\n" +
+                "<body>\n" +
+                "<div class=\"panel1\">\n" +
+                "    <h3 id=\"banner\">Si no has realizado tú solicitud para ser parte del programa de apoyo alimetario,\n" +
+                "        puedes hacerla aquí:</h3>\n";
+                if (rol.equalsIgnoreCase("estudiante")) {
+                 pagina +=   "    <a href=\"realizarSolicitud.html\"><button id=\"registroSolicitud\" class=\"ingresar\" type=\"submit\" value=\"guardar\">Realizar Solicitud</button></a>\n";
+                }else if(rol.equalsIgnoreCase("admin") || rol.equalsIgnoreCase("revisor")){
+                    pagina +=   "    <a href=\"#\"><button id=\"registroSolicitud\" class=\"ingresar\" disabled type=\"submit\" value=\"guardar\">Realizar Solicitud</button></a>\n";
+                }
+                pagina +="</div>\n" +
+                "</body>\n" +
+                "</html>\n";
+
+
+        return pagina;
     }
 }
